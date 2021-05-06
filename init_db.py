04f97d97e349c9e172044e3d4dd91ff276fbde94
chazzox <caylott@outlook.com>
2021-05-06 22:45:45 +0100

    formatted files and added darkmode

--- init_db.py
+++ init_db.py
@@ -3,7 +3,7 @@ import sqlite3
 DATABASE_FILE = "database.db"
 
 # important:
-#-------------------------------------------------------------
+# -------------------------------------------------------------
 # This script initialises your SQLite database for you, just
 # to get you started... there are better ways to express the
 # data you're going to need... especially outside SQLite.
@@ -14,14 +14,13 @@ DATABASE_FILE = "database.db"
 # Also... the name of the database (here, in SQLite, it's a
 # filename) appears in more than one place in the project.
 # That doesn't feel right, does it?
-#-------------------------------------------------------------
-
+# -------------------------------------------------------------
 connection = sqlite3.connect(DATABASE_FILE)
-print("- Opened database successfully in file \"{}\"".format(DATABASE_FILE))
+print('- Opened database successfully in file "{}"'.format(DATABASE_FILE))
 
 # using Python's triple-quote for multi-line strings:
-
-connection.execute("""
+connection.execute(
+    """
 
   CREATE TABLE IF NOT EXISTS buggies (
     id                    INTEGER PRIMARY KEY,
@@ -31,20 +30,21 @@ connection.execute("""
     flag_pattern          VARCHAR(20)
   )
 
-""")
+"""
+)
 
-print("- Table \"buggies\" exists OK")
+print('- Table "buggies" exists OK')
 
 cursor = connection.cursor()
 
 cursor.execute("SELECT * FROM buggies LIMIT 1")
 rows = cursor.fetchall()
 if len(rows) == 0:
-  cursor.execute("INSERT INTO buggies (qty_wheels) VALUES (4)")
-  connection.commit()
-  print("- Added one 4-wheeled buggy")
+    cursor.execute("INSERT INTO buggies (qty_wheels) VALUES (4)")
+    connection.commit()
+    print("- Added one 4-wheeled buggy")
 else:
-  print("- Found a buggy in the database, nice")
+    print("- Found a buggy in the database, nice")
 
 print("- OK, your database is ready")
 
