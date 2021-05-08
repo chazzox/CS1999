b9e80d56664c6ba3e9c7c43dcd031683716ede54
chazzox <caylott@outlook.com>
2021-05-08 19:33:25 +0100

    removed replit file, added new form data and basic server side validation

--- app.py
+++ app.py
@@ -1,55 +1,69 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3 as sql
 
-# app - The flask application where all the magical things are configured.
+# Initialise websever
 app = Flask(__name__)
 
-# Constants - Stuff that we need to know that won't ever change!
+# CONTANTS
 DATABASE_FILE = "database.db"
 DEFAULT_BUGGY_ID = "1"
 BUGGY_RACE_SERVER_URL = "http://rhul.buggyrace.net"
 
-# ------------------------------------------------------------
-# the index page
-# ------------------------------------------------------------
+
 @app.route("/")
 def home():
     return render_template("index.html", server_url=BUGGY_RACE_SERVER_URL)
 
 
-# ------------------------------------------------------------
-# creating a new buggy:
-#  if it's a POST request process the submitted data
-#  but if it's a GET request, just show the form
-# ------------------------------------------------------------
 @app.route("/new", methods=["POST", "GET"])
 def create_buggy():
     if request.method == "GET":
         return render_template("buggy-form.html")
     elif request.method == "POST":
+        # TODO: rewrite the validation method and create error message. Maybe use class?
+        valid = True
         msg = ""
-        qty_wheels = request.form["qty_wheels"]
         print(request.form)
-        try:
-            with sql.connect(DATABASE_FILE) as con:
-                cur = con.cursor()
-                cur.execute(
-                    "UPDATE buggies set qty_wheels=? WHERE id=?",
-                    (qty_wheels, DEFAULT_BUGGY_ID),
-                )
-                con.commit()
-                msg = "Record successfully saved"
-        except:
-            con.rollback()
-            msg = "error in update operation"
-        finally:
-            con.close()
+        # get our values from the form that we want to add to the database
+        qty_wheels = request.form["qty_wheels"]
+        power_type = request.form["power_type"]
+        # TODO: improve this god awful way of validating form data
+        valid = qty_wheels.strip().isdigit() and (
+            power_type
+            in [
+                "petrol",
+                "fusion",
+                "steam",
+                "bio",
+                "electric",
+                "rocket",
+                "hamster",
+                "thermo",
+                "solar",
+                "wind",
+            ]
+        )
+        if valid:
+            try:
+                with sql.connect(DATABASE_FILE) as con:
+                    cur = con.cursor()
+                    cur.execute(
+                        "UPDATE buggies set qty_wheels=?, power_type=? WHERE id=?",
+                        (qty_wheels, power_type, DEFAULT_BUGGY_ID),
+                    )
+                    con.commit()
+                    msg = "Record successfully saved"
+            except Exception as e:
+                print(e)
+                con.rollback()
+                msg = "error in update operation"
+            finally:
+                con.close()
+        else:
+            msg = "data was not valid"
         return render_template("updated.html", msg=msg)
 
 
-# ------------------------------------------------------------
-# a page for displaying the buggy
-# ------------------------------------------------------------
 @app.route("/buggy")
 def show_buggies():
     con = sql.connect(DATABASE_FILE)
@@ -60,24 +74,11 @@ def show_buggies():
     return render_template("buggy.html", buggy=record)
 
 
-# ------------------------------------------------------------
-# a placeholder page for editing the buggy: you'll need
-# to change this when you tackle task 2-EDIT
-# ------------------------------------------------------------
 @app.route("/edit")
 def edit_buggy():
     return render_template("buggy-form.html")
 
 
-# ------------------------------------------------------------
-# You probably don't need to edit this... unless you want to ;)
-#
-# get JSON from current record
-#  This reads the buggy record from the database, turns it
-#  into JSON format (excluding any empty values), and returns
-#  it. There's no .html template here because it's *only* returning
-#  the data, so in effect jsonify() is rendering the data.
-# ------------------------------------------------------------
 @app.route("/json")
 def summary():
     con = sql.connect(DATABASE_FILE)
