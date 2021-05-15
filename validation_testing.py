40be2a955397a11f2b81e61f9182b19c740609a8
chazzox <caylott@outlook.com>
2021-05-15 01:20:11 +0100

    added basic schema validation files

---
+++ validation_testing.py
@@ -0,0 +1,91 @@
+from jsonschema import validate
+
+schema = {
+    "type": "object",
+    "properties": {
+        "id": {"title": "Id of buggy", "type": "integer"},
+        "qty_wheels": {
+            "title": "Number of wheels",
+            "type": "integer",
+            "minimum": 4,
+            "multipleOf": 2,
+        },
+        "power_type": {
+            "title": "Primary motive power",
+            "enum": [
+                "petrol",
+                "fusion",
+                "steam",
+                "bio",
+                "electric",
+                "rocket",
+                "hamster",
+                "thermo",
+                "solar",
+                "wind",
+            ],
+        },
+        "power_units": {
+            "title": "Primary motive power units",
+            "type": "integer",
+            "multipleOf": 2,
+        },
+        "aux_power_type": {
+            "title": "Auxiliary motive power",
+            "enum": [
+                "petrol",
+                "fusion",
+                "steam",
+                "bio",
+                "electric",
+                "rocket",
+                "hamster",
+                "thermo",
+                "solar",
+                "wind",
+            ],
+        },
+        "aux_power_units": {"title": "Auxiliary motive power units", "type": "integer"},
+        "hamster_booster": {"title": "Hamster booster", "type": "integer"},
+        "flag_pattern": {
+            "title": "Flag's pattern",
+            "enum": ["plain", "vstripe", "hstripe", "dstripe", "checker", "spot"],
+        },
+        "flag_color": {"title": "Flag's colour", "type": "string"},
+        "flag_color_secondary": {"title": "Flag's pattern", "type": "string"},
+        "tyres": {
+            "title": "Type of tyres",
+            "enum": ["knobbly", "slick", "steelband", "reactive", "maglev"],
+        },
+        "qty_tyres": {"title": "Number of tyres", "type": "integer", "minimum": 4},
+        "armour": {
+            "title": "Armour",
+            "enum": ["wood", "aluminium", "thinsteel", "thicksteel", "titanium"],
+        },
+        "attack": {
+            "title": "Offensive capability",
+            "enum": ["spike", "flame", "charge", "biohazard"],
+        },
+        "qty_attacks": {"title": "Number of attacks", "type": "integer"},
+        "fireproof": {"title": "Fireproof?", "type": "boolean"},
+        "insulated": {"title": "Insulated?", "type": "boolean"},
+        "antibiotic": {"title": "Antibiotic?", "type": "boolean"},
+        "banging": {"title": "Banging sound system?", "type": "boolean"},
+        "algo": {
+            "title": "Race computer algorithm",
+            "enum": [
+                "defensive",
+                "steady",
+                "offensive",
+                "titfortat",
+                "random",
+                "buggy",
+            ],
+        },
+    },
+    "required": [],
+    "additionalProperties": False,
+    "$id": "buggy",
+}
+
+print(validate(instance={"id": 2, "qty_wheels": 4}, schema=schema))
