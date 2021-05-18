5f9c43afbbe3cf4e81e4e056c9ab93b3780015c7
chazzox <42206116+chazzox@users.noreply.github.com>
2021-05-18 16:01:00 +0100

    added rest of properties to the database init

--- init_db.py
+++ init_db.py
@@ -8,11 +8,22 @@ print('- Opened database successfully in file "{}"'.format(DATABASE_FILE))
 connection.execute(
     """
   	CREATE TABLE IF NOT EXISTS buggies (
-    id                    INTEGER PRIMARY KEY,
-    qty_wheels            INTEGER DEFAULT 4,
-    flag_color            VARCHAR(20),
-    flag_color_secondary  VARCHAR(20),
-    flag_pattern          VARCHAR(20)
+    id                    	INTEGER PRIMARY KEY,
+    qty_wheels            	INTEGER DEFAULT 4,
+	power_type				VARCHAR(20),
+	power_units				INTEGER DEFAULT 1,
+	aux_power_type			VARCHAR(20),
+	aux_power_units			INTEGER DEFAULT 1,
+	hamster_booster 		INTEGER DEFAULT 0,
+    flag_color            	VARCHAR(20),
+    flag_pattern          	VARCHAR(20),
+    flag_color_secondary  	VARCHAR(20),
+	tyres					VARCHAR(20),˝
+	qty_tyres 				INTEGER DEFAULT 4,
+	armour					VARCHAR(20),
+	attack					varchar(20),
+	qty_attacks				INTEGER DEFAULT 0,
+	algo					VARCHAR(20)
   )
 """
 )
