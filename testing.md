eba746ba76b814e8c3425dd3dee4956316aa94cc
chazzox <caylott@outlook.com>
2021-06-04 14:50:09 +0100

    updated the test script and readme

--- testing.md
+++ testing.md
@@ -1,6 +1,9 @@
 # TESTING
 
-This project contains a file called `testing.sh`
-to run this file correctly please first make sure that the url at the top of the file matches the one you use to run the server on (port also needs to match!)
+This project contains a file called `testing.sh` to run this file correctly please
+first make sure that the url at the top of the file matches the one you use to run
+the server on (port also needs to match!)
 
-Simply run `sh testing.sh` to start the tests, the program will tell you the tests pass/fail.
+-   First you need to start up the python server `python app.py`
+-   Then create a new terminal tab and run `sh testing.sh` to start the tests, the
+    program will tell you the tests pass/fail.
--- testing.sh
+++ testing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-URL='0.0.0.0:5000'
+URL='127.0.0.1:5000'
 
 echo "Testing Script"
 echo "This file will test api reuqest to the server, it will consider a request a 'success' if it returns the expected status code"
@@ -9,8 +9,7 @@ echo "This file will test api reuqest to the server, it will consider a request 
 echo 'Testing a post request with nothing in (Expecting code 400)'
 DATA=$(curl -si -X POST "$URL/new" | grep 'HTTP/1.0')
 if [[ $DATA =~ "400" ]];  then
-    echo $DATA
-    echo "TEST PASSED MOVING ON."
+    echo "$DATA - TEST PASSED MOVING ON."
 else
     echo "TEST FAILED, PLEASE FIX."
 fi
@@ -18,8 +17,7 @@ fi
 echo 'Testing a post request with missing keys (Expecting code 400)'
 DATA=$(curl -si -X POST -d 'qty_wheels=4&algo=steady&flag_pattern=plain&qty_tyres=6' "$URL/new" | grep 'HTTP/1.0')
 if [[ $DATA =~ "400" ]];  then
-    echo $DATA
-    echo "TEST PASSED MOVING ON."
+    echo "$DATA - TEST PASSED MOVING ON."
 else
     echo "TEST FAILED, PLEASE FIX."
 fi
@@ -28,8 +26,7 @@ fi
 echo 'Testing a post request with invalid data (Expecting code 400)'
 DATA=$(curl -si -X POST -d 'qty_wheels=foo&power_type=bar&power_units=green&aux_power_type=some&aux_power_units=0&hamster_booster=0&flag_color=#000000&flag_pattern=plain&flag_color_secondary=#000000&tyres=knobbly&qty_tyres=4&armour=None&attack=None&qty_attacks=0&fireproof=0&insulated=0&antibiotic=test&banging=0&algo=offensive' "$URL/new" | grep 'HTTP/1.0')
 if [[ $DATA =~ "400" ]];  then
-    echo $DATA
-    echo "TEST PASSED MOVING ON."
+	echo "$DATA - TEST PASSED MOVING ON."
 else
     echo "TEST FAILED, PLEASE FIX."
 fi
@@ -37,8 +34,7 @@ fi
 echo 'Testing a post request with valid data (Expecting code 200)'
 DATA=$(curl -si -X POST -d 'qty_wheels=4&power_type=petrol&power_units=1&aux_power_type=None&aux_power_units=0&hamster_booster=0&flag_color=#000000&flag_pattern=plain&flag_color_secondary=#000000&tyres=knobbly&qty_tyres=4&armour=None&attack=None&qty_attacks=0&fireproof=0&insulated=0&antibiotic=0&banging=0&algo=offensive' "$URL/new" | grep 'HTTP/1.0')
 if [[ $DATA =~ "200" ]];  then
-    echo $DATA
-    echo "TEST PASSED MOVING ON."
+    echo "$DATA - TEST PASSED MOVING ON."
 else
     echo "TEST FAILED, PLEASE FIX."
 fi
